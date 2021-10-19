7ecaa808fff4b360f00f9580b9daa1633ebd7340
brody <947431990@qq.com>
2021-10-19 20:31:46 +0800

    优化 xirr 工具类

--- src/main/java/cn/brody/financing/util/IrrUtil.java
+++ src/main/java/cn/brody/financing/util/IrrUtil.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 /**
  * 计算 xirr 和 irr 的工具类
+ *
  * @author brody
  * @date 2021/10/19
  */
@@ -18,34 +19,40 @@ public class IrrUtil {
     private static final double X0 = 0.5D;
 
     /**
-     *  计算 XIRR
-     *  【参考列表】
-     *  IRR Excel:https://support.office.com/zh-cn/article/IRR-%E5%87%BD%E6%95%B0-64925eaa-9988-495b-b290-3ad0c163c1bc
-     *  XIRR Excel:https://support.office.com/zh-cn/article/XIRR-%E5%87%BD%E6%95%B0-de1242ec-6477-445b-b11b-a303ad9adc9d
+     * 计算 XIRR
+     * 【参考列表】
+     * IRR Excel:https://support.office.com/zh-cn/article/IRR-%E5%87%BD%E6%95%B0-64925eaa-9988-495b-b290-3ad0c163c1bc
+     * XIRR Excel:https://support.office.com/zh-cn/article/XIRR-%E5%87%BD%E6%95%B0-de1242ec-6477-445b-b11b-a303ad9adc9d
+     *
      * @param list 现金流
-     * @param days 计算周期
      * @return
      */
+    public static Double xirr(List<Transaction> list) {
+        return xirr(list, X0);
+    }
+
     public static Double xirr(List<Transaction> list, double guess) {
-        return  Xirr.builder().withTransactions(list).withGuess(guess).xirr();
+        return Xirr.builder().withTransactions(list).withGuess(guess).xirr();
     }
 
     /**
      * IRR
+     *
      * @param list
      * @return
      */
     public static Double irr(List<Transaction> list) {
-        return irr(list,X0);
+        return irr(list, X0);
     }
 
     /**
      * 计算IRR
      * 【参考列表】
-     *  IRR Excel:https://support.office.com/zh-cn/article/IRR-%E5%87%BD%E6%95%B0-64925eaa-9988-495b-b290-3ad0c163c1bc
-     *  XIRR Excel:https://support.office.com/zh-cn/article/XIRR-%E5%87%BD%E6%95%B0-de1242ec-6477-445b-b11b-a303ad9adc9d
-     *  @param list 现金流
-     *  @param guess 猜测值
+     * IRR Excel:https://support.office.com/zh-cn/article/IRR-%E5%87%BD%E6%95%B0-64925eaa-9988-495b-b290-3ad0c163c1bc
+     * XIRR Excel:https://support.office.com/zh-cn/article/XIRR-%E5%87%BD%E6%95%B0-de1242ec-6477-445b-b11b-a303ad9adc9d
+     *
+     * @param list  现金流
+     * @param guess 猜测值
      */
     public static Double irr(List<Transaction> list, double guess) {
         double irrResult = Double.NaN;
