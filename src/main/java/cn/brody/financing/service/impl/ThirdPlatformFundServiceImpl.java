6357c8832e1affb90edee75dd51fcd8e40592754
BrodyChen <947431990@qq.com>
2025-02-13 23:17:24 +0800

    使用aktool获取基金概况，并使用缓存

--- src/main/java/cn/brody/financing/service/impl/ThirdPlatformFundServiceImpl.java
+++ src/main/java/cn/brody/financing/service/impl/ThirdPlatformFundServiceImpl.java
@@ -1,12 +1,11 @@
 package cn.brody.financing.service.impl;
 
 import cn.brody.financing.pojo.aktool.AktoolFundNetValueVO;
-import cn.brody.financing.pojo.mairui.MairuiFundOverviewVO;
+import cn.brody.financing.pojo.aktool.AktoolFundOverviewVO;
 import cn.brody.financing.pojo.vo.FundNetValueVO;
 import cn.brody.financing.pojo.vo.FundOverviewVO;
 import cn.brody.financing.service.IThirdPlatformFundService;
 import cn.brody.financing.utils.AktoolUtils;
-import cn.brody.financing.utils.MairuiUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +24,7 @@ public class ThirdPlatformFundServiceImpl implements IThirdPlatformFundService {
 
     @Override
     public FundOverviewVO getFundOverview(String fundCode) {
-        MairuiFundOverviewVO fundOverviewVO = MairuiUtils.getFundOverviewVO(fundCode);
+        AktoolFundOverviewVO fundOverviewVO = AktoolUtils.getFundOverviewVO(fundCode);
         return new FundOverviewVO(fundOverviewVO);
     }
 
@@ -40,12 +39,8 @@ public class ThirdPlatformFundServiceImpl implements IThirdPlatformFundService {
 
     @Override
     public FundNetValueVO getFundNetValue(String fundCode, String tradeDate) {
-        // MairuiOpenFundLatestNetValueVO fundNetValueLatestVO = MairuiUtils.getFundNetValueLatestVO(fundCode);
-        // return new FundNetValueVO(fundNetValueLatestVO);
-        // 下面是Aktool的方式获取
         FundOverviewVO fundOverview = getFundOverview(fundCode);
         List<AktoolFundNetValueVO> openFundNetValueFullList = AktoolUtils.getOpenFundNetValueFull(fundCode);
-        // TODO: BrodyChen 2025/1/10 这里应该使用给定的日期获取的，我图省事儿，直接用了最后一个也即最新的，这里需要修改
         AktoolFundNetValueVO aktoolFundNetValueVO = openFundNetValueFullList.get(openFundNetValueFullList.size() - 1);
         return new FundNetValueVO(aktoolFundNetValueVO, fundCode, fundOverview.getFundName());
     }
